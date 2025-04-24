671c1a0eef40374da4a30a7bad8603f0c217cf66
Tasnim Mohammed <101313741+TasnimMo7amed@users.noreply.github.com>
2025-04-24 21:59:26 +0300

    Add files via upload

---
+++ Sales Analysis/DIM_Calendar.sql
@@ -0,0 +1,27 @@
+-- Cleansed DIM_Date Table --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+   Left([EnglishMonthName], 3) AS MonthShort,
+  --[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day, 
+  --[SpanishDayNameOfWeek], 
+  --[FrenchDayNameOfWeek], 
+  --[DayNumberOfMonth], 
+  --[DayNumberOfYear], 
+  --[WeekNumberOfYear],
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,  
+  --[SpanishMonthName], 
+  --[FrenchMonthName], 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  --[CalendarSemester], 
+  --[FiscalQuarter], 
+  --[FiscalYear], 
+  --[FiscalSemester] 
+FROM 
+ [AdventureWorksDW2019].[dbo].[DimDate]
+ WHERE 
+  CalendarYear >= 2019
