c2b92fdb6658f0232b59da71cee0f4a5fc8520f4
Tasnim Mohammed <101313741+TasnimMo7amed@users.noreply.github.com>
2025-05-14 16:28:44 +0300

    Add files via upload

---
+++ Marketing Analytics/Customer Reviews.sql
@@ -0,0 +1,12 @@
+-- Query to clean whitespace issues in the ReviewText column
+
+SELECT 
+    ReviewID,  -- Selects the unique identifier for each review
+    CustomerID,  -- Selects the unique identifier for each customer
+    ProductID,  -- Selects the unique identifier for each product
+    ReviewDate,  -- Selects the date when the review was written
+    Rating,  -- Selects the numerical rating given by the customer (e.g., 1 to 5 stars)
+	--ReviewText,
+    REPLACE(ReviewText, '  ', ' ') AS ReviewText
+FROM 
+    dbo.customer_reviews;  -- Specifies the source table from which to select the data
